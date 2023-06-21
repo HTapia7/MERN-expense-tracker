f1c188958db36c4e7c6f8787be5d766561f4c9d5
Hernan Tapia <soliditize@MacBook-Pro.local>
2023-06-20 18:09:25 -0700

    added routes for delete and update

--- server/model/routes/expenses.js
+++ server/model/routes/expenses.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ExpenseModel } from "../Expenses.js"; 
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -12,13 +13,64 @@ router.get("/expenses" , async (req, res) => {
 router.post("/add-expense" , async (req , res) => {
     const { text , amount } = req.body
     
-    const expense = new ExpenseModel( { text , amount})
+    const expense = new ExpenseModel( { text , amount })
     await expense.save()
 
     res.json({ message: "New Expense added"})
 });
 
-router.delete("/delete-expense" , async ( req , res ) => {
+router.delete("/delete-expense/:id" , async ( req , res ) => {
+
+    try {
+        const { id } = req.params
+
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400)
+            .json({ error: "Invalid Id" });
+        }
+
+        const deleteExpense = await ExpenseModel.findByIdAndDelete(id);
+
+        if(!deleteExpense){
+            return res.status(400)
+            .json({ error: "Item not found"});
+        }
+        return res.status(200)
+        .json({ message: "Item deleted"})
+
+    } catch (error) {
+        return res.status(500)
+        .json({ error: "server error"})
+    }
+
+});
+
+router.put("/update-expense/:id" , async ( req , res ) => {
+
+    try {
+        const { id } = req.params
+        const updateExpense = req.body
+
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400)
+            .json({ error: "Invalid Id" });
+        }
+
+        const newExpense = await ExpenseModel.findByIdAndUpdate(id , updateExpense , {
+            new: true,
+        });
+
+        if(!newExpense){
+            return res.status(400)
+            .json({ error: "ID is invalid"});
+        }
+        return res.status(200)
+        .json({ message: "Item Updated"})
+
+    } catch (error) {
+        return res.status(500)
+        .json({ error: "server error"})
+    }
 
 });
 
